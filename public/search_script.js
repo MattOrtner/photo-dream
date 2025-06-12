026a1ea73e78006178f6648efa875f3f75883e72
Matt Ortner <matthewortner@gmail.com>
2025-06-12 17:44:26 -0400

    fix: ensure correct href and id assignment for empty search input

--- public/search_script.js
+++ public/search_script.js
@@ -31,7 +31,10 @@ document.getElementById("searchButton").addEventListener("click", () => {
       const previousSearch = document.createElement("a");
       previousSearch.className = "previous-search";
       previousSearch.innerHTML = searchInput.value || "easter egg";
-      previousSearch.href = `#${searchInput.value}` || "#easter egg";
+      previousSearch.href = `#${searchInput.value}`;
+      if (searchInput.value === "") {
+        previousSearch.href = "#easter egg";
+      }
       previousSearchContainer.prepend(previousSearch);
 
       const photoGroupsContainer = document.getElementById(
@@ -39,7 +42,10 @@ document.getElementById("searchButton").addEventListener("click", () => {
       );
       const photoGroup = document.createElement("div");
       photoGroup.className = "photo-group";
-      photoGroup.id = `${searchInput.value}` || "easter egg";
+      photoGroup.id = `${searchInput.value}`;
+      if (searchInput.value === "") {
+        photoGroup.id = "easter egg";
+      }
 
       data.results.forEach((photo, i) => {
         const photoContainer = document.createElement("div");
